57788442920139868cb000564c09820fc395bdb4
afrobetclub <alexi.robin@gmail.com>
2026-07-10 14:49:21 +0200

    Add files via upload

---
+++ afrobet-archives-v15/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRJH1dw4bmYwPC5Yxf8zKEeQvOb7aWKnxoZ-XTuPfBn_tOVyPQjQdE-kcBcOGRzQ-Iy_1FaDvGoPjV_/pub?gid=622475547&single=true&output=csv",
+  MAX_ROWS: 999999
+};
